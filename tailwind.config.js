95a1b1449cca5c818605fac4bf9af29c4fc2341f
hbuhrer <156240575+hbuhrer@users.noreply.github.com>
2024-02-20 19:02:49 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./scr/**/*.html"],
+  theme: {
+
+    colors:{
+      'primary': '#FF6363',
+      'secondary': {
+        100: '#212020',
+        200: '#888883',
+      }
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
